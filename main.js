46050712df13e27a3b381a628681b867b4e80590
= <=>
2022-03-23 16:45:39 +0700

    Add new project food

---
+++ main.js
@@ -0,0 +1,10 @@
+var loginModal = document.querySelector('#myLoginModal');
+var loginIcon = document.querySelector('#loginModalIcon');
+
+function myModal() {
+    loginModal.style.display = 'block';
+}
+
+function myClose() {
+    loginModal.style.display = 'none';
+}
